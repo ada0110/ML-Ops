f62ddbfc30a3bc6eaa7b3824ae608c103165b217
ada0110 <64487038+ada0110@users.noreply.github.com>
2021-09-23 19:17:43 +0530

    saving best model

--- src/gamma_plot_digits_classification.py
+++ src/gamma_plot_digits_classification.py
@@ -12,6 +12,7 @@ print(__doc__)
 # Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
 # License: BSD 3 clause
 
+import pickle
 # Standard scientific Python imports
 import matplotlib.pyplot as plt
 
@@ -69,20 +70,10 @@ for ax, image, label in zip(axes, digits.images, digits.target):
 
 
 
-def digitsClassifier(data, gamma=0.001):
+def digitsClassifier(gamma=0.001):
     #print("\n\ndata shape:", data.shape)
     #print("train-test split is:", 1-test_size,":",test_size, "\n\n")
 
-    # flatten the images
-    n_samples = len(data)
-    data = data.reshape((n_samples, -1))
-
-    # split data into 70% train and 30% (test + val) subsets
-    X_train, X_test, y_train, y_test = train_test_split(
-        data, digits.target, test_size=0.3, shuffle=False)
-    
-    # split test into test(15%) and val(15%)
-    X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.5, shuffle=False)
 
     # printing the shapes
     # print(f"X_train:{X_train.shape}, X_test:{X_test.shape}, \ny_train:{y_train.shape},\
@@ -102,11 +93,12 @@ def digitsClassifier(data, gamma=0.001):
     f1_train = round(f1_score(y_train, predicted, average='macro', zero_division=0), 4)
 
     # Predict the value of the digit on the test subset
-    predicted = clf.predict(X_test)
-    a_test = round(accuracy_score(y_test, predicted), 4)
-    p_test = round(precision_score(y_test, predicted, average='macro', zero_division=0), 4)
-    r_test = round(recall_score(y_test, predicted, average='macro', zero_division=0), 4)
-    f1_test = round(f1_score(y_test, predicted, average='macro', zero_division=0), 4)    
+    # predicted = clf.predict(X_test)
+    # a_test = round(accuracy_score(y_test, predicted), 4)
+    # p_test = round(precision_score(y_test, predicted, average='macro', zero_division=0), 4)
+    # r_test = round(recall_score(y_test, predicted, average='macro', zero_division=0), 4)
+    # f1_test = round(f1_score(y_test, predicted, average='macro', zero_division=0), 4)    
+    
 
     # Predict the value of the digit on the validation subset
     predicted = clf.predict(X_val)
@@ -115,37 +107,92 @@ def digitsClassifier(data, gamma=0.001):
     r_val = round(recall_score(y_val, predicted, average='macro', zero_division=0), 4)
     f1_val = round(f1_score(y_val, predicted, average='macro', zero_division=0), 4)
 
-    return [[a_train, p_train, r_train, f1_train], [a_test, p_test, r_test, f1_test], [a_val, p_val, r_val, f1_val]]
+
+    return clf, [[a_train, p_train, r_train, f1_train], [a_val, p_val, r_val, f1_val]]
     
 
+# take gamma as command line argument 0
+# gammas = list(map(float, input().split()))
+# print("gamma", gammas)
+
+
 # checking for different gamma values
-data_org = digits.images
+data = digits.images
+
+# flatten the images
+n_samples = len(data)
+data = data.reshape((n_samples, -1))
+
+# split data into 70% train and 30% (test + val) subsets
+X_train, X_test, y_train, y_test = train_test_split(data, digits.target, test_size=0.3, shuffle=False)
+
+# split test into test(15%) and val(15%)
+X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.5, shuffle=False)
+
+
 gammas =  [0.000005, 0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1]
 results_gamma = []
-
 best_f1 = -1
 gamma_opt = -1
 best_metrics = []
 
+
 for gamma in gammas:
-    res = digitsClassifier(data_org, gamma=gamma)
+    clf, res = digitsClassifier(gamma=gamma)
+
+    # skippping gammas where accuracy is less than 0.11
+    if res[1][3] < 0.11:
+        print(f"\nskipping for {gamma}\n")
+        continue 
+
     results_gamma.append(res)
 
     print(f"\ngamma: {gamma}")
-    for s,r in zip(["train", "test", "val"], res):
+    for s,r in zip(["train", "val"], res):
         print(f"\t{s + ' scores:':<15} {r}")
 
     # validation f1 is in 3rd row, 4th col
-    if res[2][3] > best_f1:
-        best_f1 = res[2][3]
+    if res[1][3] > best_f1:
+        best_f1 = res[1][3]
         gamma_opt = gamma
         best_metrics = res
+        best_clf = clf
 
-print(f"\n\nbest validation f1 score is {best_f1} for optimal gamma {gamma_opt}\n")
-    
-print(f"Optimal gamma: {gamma_opt}")
-for s,r in zip(["train", "test", "val"], best_metrics):
-    print(f"\t{s + ' scores:':<15} {r}")
+print("Saving the best model...")
+save_file = open(f'/home/ada/codes/ML-Ops_Scikit/models/best_clf_{best_f1}.pkl', 'wb')
+pickle.dump(best_clf, save_file)
+save_file.close()
+
+
+# should run only for best gamma 
+predicted = best_clf.predict(X_test)
+a_test = round(accuracy_score(y_test, predicted), 4)
+p_test = round(precision_score(y_test, predicted, average='macro', zero_division=0), 4)
+r_test = round(recall_score(y_test, predicted, average='macro', zero_division=0), 4)
+f1_test = round(f1_score(y_test, predicted, average='macro', zero_division=0), 4)    
+
+print(f"\n\nbest validation f1 score is {best_f1} for optimal gamma {gamma_opt}") 
+print(f"\ttest scores:   {[a_test, p_test, r_test, f1_test]}\n\n")
+
+# for s,r in zip(["train", "test", "val"], best_metrics):
+#     print(f"\t{s + ' scores:':<15} {r}")
+
+# remove for loop (for gamma in gammas) and take gamma as command line argument 
 
 
 
+# loading the saved model
+print("loading the model:")
+load_file = open(f'/home/ada/codes/ML-Ops_Scikit/models/best_clf_{best_f1}.pkl', 'rb')
+loaded_model = pickle.load(load_file)
+print(loaded_model)
+
+print("\nPredicting from loaded model:")
+# should run only for best gamma 
+predicted = loaded_model.predict(X_test)
+a_test = round(accuracy_score(y_test, predicted), 4)
+p_test = round(precision_score(y_test, predicted, average='macro', zero_division=0), 4)
+r_test = round(recall_score(y_test, predicted, average='macro', zero_division=0), 4)
+f1_test = round(f1_score(y_test, predicted, average='macro', zero_division=0), 4)    
+
+print(f"\ttest scores:   {[a_test, p_test, r_test, f1_test]}\n\n")
