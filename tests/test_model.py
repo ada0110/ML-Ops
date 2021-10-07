a576b6933199e4ffd2434cef8dd10555bcee9a47
ada0110 <64487038+ada0110@users.noreply.github.com>
2021-10-07 23:52:53 +0530

    test overfitting with random classifier

--- tests/test_model.py
+++ tests/test_model.py
@@ -10,6 +10,18 @@ from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_sc
 from src.utils import preprocess, data_split, get_scores, digitsClassifier, save_model
 
 
+# random classifier
+def get_random_scores(x, y):
+    # predict randomly from 0 to 9
+    predicted = np.random.randint(10, size=len(x))
+    a = round(accuracy_score(y, predicted), 4)
+    p = round(precision_score(y, predicted, average='macro', zero_division=0), 4)
+    r = round(recall_score(y, predicted, average='macro', zero_division=0), 4)
+    f1 = round(f1_score(y, predicted, average='macro', zero_division=0), 4)
+
+    return [a, p, r, f1]
+
+
 def test_model_writing():
     # data
     digits = datasets.load_digits()
@@ -51,68 +63,14 @@ def test_small_data_overfit_checking():
      
     # get_scores returns list of [acc, precision, recall, f1]
     train_scores = get_scores(clf, x_train, y_train)
+    random_scores = get_random_scores(x_train, y_train)
 
     print("train:", train_scores)
+    print("random:", random_scores)
 
     assert train_scores[0] > threshold, "didn't overfit as per accuracy metric!"
     assert train_scores[3] > threshold, "didn't overfit as per f1 metric!"
 
-
-# """
-# # TODO: write  a test case to check if model is successfully getting created or not?
-# def test_model_writing():
-
-#     1. create some data
-
-#     2. run_classification_experiment(data, expeted-model-file)
-
-#     assert os.path.isfile(expected-model-file)
-
-
-# # TODO: write a test case to check fitting on training -- litmus test.
-
-# def test_small_data_overfit_checking():
-
-#     1. create a small amount of data / (digits / subsampling)
-
-#     2. train_metrics = run_classification_experiment(train=train, valid=train)
-
-#     assert train_metrics['acc']  > some threshold
-
-#     assert train_metrics['f1'] > some other threshold
-# """
-
-# def test_preprocess():
-#     pass
-
-
-# def test_data_split():
-#     pass
-
-
-# def test_get_scores():
-#     pass
-
-# # experiment within test case where model should be created at some place
-# # method to save model
-# # given training and validation --> write model onto disk
-# # test case to check if model is gettibng created correctly
-
-# # model should overfit on training data 
-# # that is perform better than random classifier --> litmus test
-# #  
-
-
-# def test_model_writing():
-#     # creating some data
-#     x = np.random.rand(1000, 8)
-#     y = np.random.randint(10, size=1000)
-#     x_train, x_test, x_val, y_train, y_test, y_val = data_split(x, y)
-#     clf = digitsClassifier(x_train, y_train)
-#     # saving model
-#     path = "/home/ada/codes/ML-Ops_Scikit/models/test.pkl"
-#     save_model(clf, path)
-#     assert os.path.isfile(path), "Model not saved"
-
-
+    assert train_scores[0] > random_scores[0], "didn't overfit as per accuracy metric when compared with random model!"
+    assert train_scores[3] > random_scores[3], "didn't overfit as per f1 metric when compared with random model!"
 
