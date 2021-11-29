0b834dbf057cad002e5575f5196182a829c1ef4a
ada0110 <64487038+ada0110@users.noreply.github.com>
2021-11-30 00:31:58 +0530

    Added docker commands

--- docker-app/app.py
+++ docker-app/app.py
@@ -1,8 +1,29 @@
 from flask import Flask
 from flask import request
-app = Flask(__name__)
+import numpy as np
+import pickle
 
+app = Flask(__name__)
 cnt_visit = 0
+
+
+best_model_svm = 'saved_models/best_model_svm.pkl'
+best_model_decision = 'saved_models/best_model_decision.pkl'
+
+def load_model(path):
+    print("\nloading the model...")
+    load_file = open(path, "rb")
+    loaded_model = pickle.load(load_file)
+    return loaded_model
+
+
+# load models
+clf_svm = load_model(best_model_svm)
+print("***svm model loaded***")
+
+clf_decision = load_model(best_model_svm)
+print("***decision tree model loaded***\n")
+
 
 @app.route('/')
 def hello_world():
@@ -13,27 +34,33 @@ def hello_world():
 
 # http://localhost:5000/svm_predict/?input=this is good
 # curl -G "http://localhost:5000/svm_predict/" --data-urlencode "input=hello world"
-@app.route('/svm_predict/')
-def svm_predict():
+@app.route('/svm_predict_get/')
+def svm_predict_get():
     input_ = request.args.get('input')
     return f"called svm_predict with input: {input_}\n"
 
 
 # call this using curl
-@app.route("/svm_predict_post", methods=['POST'])
-def svm_predict_post():
+@app.route("/svm_predict/", methods=['POST'])
+def svm_predict():
     image = request.json["image"]
 
-    # image = np.array(image).reshape(1, -1)
-    # prediction = clf.predict(image)
-    # print("image:\n", image)
-    # print("prediction:", prediction[0], end="\n\n")
-    # return str(prediction[0])
+    image = np.array(image).reshape(1, -1)
+    prediction = clf_svm.predict(image)
+    print("input image:\n", image)
+    return f"svm prediction: {str(prediction[0])}\n\n"
 
-    return f"called svm_predict_post with input: {image}\n"
+
+# call this using curl
+@app.route("/decision_tree_predict/", methods=['POST'])
+def decision_tree_predict():
+    image = request.json["image"]
+
+    image = np.array(image).reshape(1, -1)
+    prediction = clf_decision.predict(image)
+    print("input image:\n", image)
+    return f"decision tree prediction: {str(prediction[0])}\n\n"
     
 
 if __name__ == '__main__':
-    # it won't be accessible at http://localhost:5000/
-    # 0.0.0.0 is just a placeholder. We need to figure out which address it is actually listening on. If we are trying to reach a server from the same machine, try http://localhost:5000/
     app.run(debug=True, host='0.0.0.0')
